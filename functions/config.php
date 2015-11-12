d188fdd0cbc6a5a5140fac3de602a87abe672259
Marco <catajoe@users.noreply.github.com>
2015-11-12 18:26:27 +0100

    Update config.php

--- functions/config.php
+++ functions/config.php
@@ -6,13 +6,11 @@ if(!isset($_SESSION))
     }
 
 // Dati connessione al database
-$db_host = 'mysql.netsons.com';       // Host - solitamente localhost
-$db_utente = 'qdytaqlz_admin';        	// Nome utente del Database
-$db_password = 'pdluniba$14B';        		// Password del Database
-$db_nomedb = 'qdytaqlz_pdl';     		// Nome del Database
+$db_host = '';          // Host - solitamente localhost
+$db_utente = '';        // Nome utente del Database
+$db_password = '';      // Password del Database
+$db_nomedb = '';     	// Nome del Database
 
-
-// Procedural style PHP.net
 
 
 $link = mysqli_connect($db_host, $db_utente, $db_password, $db_nomedb);
